7676cfb4e319757356f7be385ceb66fef67156d9
hira8969 <hk2493200@gmail.com>
2026-04-18 10:53:43 +0530

    Initial commit

--- backend/app.py
+++ backend/app.py
@@ -2,18 +2,20 @@ from flask import Flask
 from flask_cors import CORS
 from models import init_data
 from controllers import auth_bp, inventory_bp, order_bp, view_bp
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='../frontend', static_folder='../frontend/static', static_url_path='/static')
 CORS(app)
-app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static', static_url_path='/static')
 
 # Register blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(inventory_bp)
 app.register_blueprint(order_bp)
 app.register_blueprint(view_bp)
+
 @app.route("/")
 def home():
     return "Backend running"
+
 if __name__ == '__main__':
     init_data()
     app.run(debug=True, port=5000)
